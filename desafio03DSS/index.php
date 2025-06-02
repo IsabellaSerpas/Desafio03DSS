468994ec13e5da333c688f8afe25b3e440512d83
IsabellaSerpas <SR240105@alumno.udb.edu.sv>
2025-06-02 15:27:25 -0600

    Delete desafio03DSS directory

--- desafio03DSS/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-
-header("Location: public/index.php");
-exit();
-?>
--- desafio03DSS/tools/generar_hash.php
+++
@@ -1,7 +0,0 @@
-<?php
-$password = "Alexa05"; // Reemplázalo con la contraseña que quieras encriptar
-$hash = password_hash($password, PASSWORD_DEFAULT);
-
-echo "Contraseña original: " . htmlspecialchars($password) . "<br>";
-echo "Hash generado: " . htmlspecialchars($hash);
-?>
